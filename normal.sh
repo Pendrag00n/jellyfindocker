bc071153bdccdc7e29382a1f597abb85d6c3eb3c
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2023-02-13 12:52:33 +0000

    Update normal.sh

--- normal.sh
+++ normal.sh
@@ -75,11 +75,61 @@ if [ ! -f /media/sample_960x400_ocean_with_audio.avi ]; then
 wget https://filesamples.com/samples/video/avi/sample_960x400_ocean_with_audio.avi -P /media
 fi
 
+# Montamos https en el servidor:
+if [ ! -d /ssl ]; then
+mkdir /ssl
+fi
+openssl genpkey -algorithm RSA -out /ssl/key.pem
+openssl req -new -x509 -key /ssl/key.pem -out /ssl/cert.pem -subj "/CN=SSCN"
+openssl pkcs12 -export -out /ssl/truecert.pfx -inkey /ssl/key.pem -in /ssl/cert.pem -passout pass:1234
+chmod 777 -R /ssl
+
+# Incluimos el certificado en la configuración del jellyfin
+cat << EOF > /etc/jellyfin/network.xml
+<?xml version="1.0" encoding="utf-8"?>
+<NetworkConfiguration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <RequireHttps>false</RequireHttps>
+  <CertificatePath>/ssl/truecert.pfx</CertificatePath>
+  <CertificatePassword>1234</CertificatePassword>
+  <BaseUrl />
+  <PublicHttpsPort>8920</PublicHttpsPort>
+  <HttpServerPortNumber>8096</HttpServerPortNumber>
+  <HttpsPortNumber>8920</HttpsPortNumber>
+  <EnableHttps>true</EnableHttps>
+  <PublicPort>8096</PublicPort>
+  <UPnPCreateHttpPortMap>false</UPnPCreateHttpPortMap>
+  <UDPPortRange />
+  <EnableIPV6>false</EnableIPV6>
+  <EnableIPV4>true</EnableIPV4>
+  <EnableSSDPTracing>false</EnableSSDPTracing>
+  <SSDPTracingFilter />
+  <UDPSendCount>2</UDPSendCount>
+  <UDPSendDelay>100</UDPSendDelay>
+  <IgnoreVirtualInterfaces>true</IgnoreVirtualInterfaces>
+  <VirtualInterfaceNames>vEthernet*</VirtualInterfaceNames>
+  <GatewayMonitorPeriod>60</GatewayMonitorPeriod>
+  <TrustAllIP6Interfaces>false</TrustAllIP6Interfaces>
+  <HDHomerunPortRange />
+  <PublishedServerUriBySubnet />
+  <AutoDiscoveryTracing>false</AutoDiscoveryTracing>
+  <AutoDiscovery>true</AutoDiscovery>
+  <RemoteIPFilter />
+  <IsRemoteIPFilterBlacklist>false</IsRemoteIPFilterBlacklist>
+  <EnableUPnP>false</EnableUPnP>
+  <EnableRemoteAccess>true</EnableRemoteAccess>
+  <LocalNetworkSubnets />
+  <LocalNetworkAddresses />
+  <KnownProxies />
+  <EnablePublishedServerUriByRequest>false</EnablePublishedServerUriByRequest>
+</NetworkConfiguration>
+EOF
+
+# Reiniciamos los servicios
 systemctl restart bind9
 systemctl restart jellyfin.service
     
     echo ""
     echo "Jellyfin instalado correctamente"
-    echo "Abre tu navegador y accede a http://$ip:8096"
+    echo "Abre tu navegador y accede a https://$ip:8920"
     echo "Tambien puedes configurar $ip como DNS y acceder a traves de www.julio.test:8096
     echo ""
